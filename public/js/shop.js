fa496ae39663f4b79337ef8ef6e6f68de001f685
Brian Shoemaker <brianshoemaker@gmail.com>
2014-09-08 11:48:12 -0500

    Additional validation logic.

--- public/js/shop.js
+++ public/js/shop.js
@@ -2,16 +2,31 @@
 /*global simpleCart:false */
 'use strict';
 
-simpleCart({
-    cartColumns: [
-        { attr: 'name' , label: 'Name' },
-        { attr: 'price' , label: 'Price', view: 'currency' },
-        { attr: 'quantity' , label: 'Qty' },
-        { attr: 'total' , label: 'SubTotal', view: 'currency' },
-        { view: 'remove' , text: 'Remove' , label: false },
-        { view: 'foo', text: '', label: false }
-    ]
-});
+$(document).ready(function () {
+    simpleCart({
+        cartColumns: [
+            { attr: 'name' , label: 'Name' },
+            { attr: 'price' , label: 'Price', view: 'currency' },
+            { attr: 'quantity' , label: 'Qty' },
+            { attr: 'total' , label: 'SubTotal', view: 'currency' },
+            { view: 'remove' , text: 'Remove' , label: false },
+            { view: 'foo', text: '', label: false }
+        ]
+    });
 
-// Reset all quantities to 1
-$('input').val(1);
+    simpleCart.bind('afterAdd', function(item) {
+        $(".alert").alert('close');
+    });
+
+    // Reset all quantities to 1
+    $('input').val(1);
+
+    $('#checkoutMain, #checkoutModal').click(function () {
+        if (simpleCart.quantity() === 0) {
+            $('#msgContainer').html('<div class="alert alert-danger" role="alert"><h4>Error</h4>You must have at least one item in your shopping cart to checkout.</div>');
+            $('#cartModal').modal('hide');
+            return false;
+        }
+    });
+    
+});
